b7260c4572919e57e01fc64036bb6ea63333ea52
mabochen <bochenma@cs.washington.edu>
2020-08-09 19:46:28 -0700

    added example and km_test

--- examples/usage.py
+++ examples/usage.py
@@ -10,4 +10,8 @@ class Usage(km_test.TestCase):
         self.SBML = helpers.getSimple()
 
     def testParameterInit(self):
+        """
+        Testing whether all of the parameters have set values
+        """
+        # calls the method from the super class that checks for initialization
         return super().assertParameterInit(self.SBML)
