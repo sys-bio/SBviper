2be6d28d79529d9cf368f456ca487ea065b1fa91
mabochen <bochenma@cs.washington.edu>
2020-09-16 16:45:29 -0700

    added design

--- src/run_models_test.py
+++ src/run_models_test.py
@@ -3,10 +3,11 @@ import util as ut
 from simple_sbml.simple_sbml import SimpleSBML
 from km_test import StaticTestCase
 
-directory = "/Users/mabochen/Desktop/Research/km_test/examples/biomodels"
+directory = "/Users/mabochen/Desktop/Research/SBViper/examples/biomodels"
 
 total = 0
 warning = 0
+names = []
 
 for filename in os.listdir(directory):
     if filename.endswith(".xml"):
@@ -18,9 +19,11 @@ for filename in os.listdir(directory):
         except (ValueError, IOError, AttributeError) as e:
             continue
         tester = StaticTestCase(sbml)
-        missing = tester.assert_reactants_in_kl()
+        missing = tester.assert_parameter_init()
         total += 1
         if len(missing) > 0:
+            names.append(filename)
             warning += 1
 print("Total: " + str(total))
-print("Warning models: " + str(warning))
+print("# of Warning models: " + str(warning))
+print("Warning models: " + str(names))
