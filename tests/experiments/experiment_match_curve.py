165903210584e3630386bfe528649199bdff57d0
stevema <bochenma@cs.uw.edu>
2020-10-31 14:09:27 -0700

    original tsc created in experiments

--- tests/experiments/experiment_match_curve.py
+++ tests/experiments/experiment_match_curve.py
@@ -6,12 +6,13 @@ import random # Generate random numbers
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pylab as plt # Additional Python plotting utilities
+from SBviper.viper_dynamic.collection_time_series import TimeSeriesCollection
 
 # model = te.loadSBMLModel("/Users/stevema/Desktop/Research/SBviper/examples/biomodels/BIOMD0000000090.xml")
 # model.exportToAntimony("model_ant_original.txt", current=True)
 
-model = te.loada("/Users/stevema/Desktop/Research/SBviper/tests/experiments/model_ant_original.txt")
+original_model = te.loada("/Users/stevema/Desktop/Research/SBviper/tests/experiments/model_ant_original.txt")
 
-model.simulate(0, 100, 1000, ['time', 'oxy'])
 # model.plot(figsize = (10, 6), xtitle = 'Time', ytitle = 'Concentration')
-
+original_result = original_model.simulate(0, 100, 1000, ['time', 'oxy'])
+original_tsc = TimeSeriesCollection.from_nd_array(original_result)
