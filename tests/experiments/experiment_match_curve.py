e3be35f9e00e98e64bd6096b42735af8924ecf10
stevema <bochenma@cs.uw.edu>
2020-10-31 14:27:49 -0700

    experiment setup complete

--- tests/experiments/experiment_match_curve.py
+++ tests/experiments/experiment_match_curve.py
@@ -7,12 +7,32 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pylab as plt # Additional Python plotting utilities
 from SBviper.viper_dynamic.collection_time_series import TimeSeriesCollection
+from SBviper.viper_dynamic.time_series import TimeSeries
 
 # model = te.loadSBMLModel("/Users/stevema/Desktop/Research/SBviper/examples/biomodels/BIOMD0000000090.xml")
 # model.exportToAntimony("model_ant_original.txt", current=True)
 
 original_model = te.loada("/Users/stevema/Desktop/Research/SBviper/tests/experiments/model_ant_original.txt")
+revised_model = te.loada("/Users/stevema/Desktop/Research/SBviper/tests/experiments/model_ant_revised.txt")
 
-# model.plot(figsize = (10, 6), xtitle = 'Time', ytitle = 'Concentration')
-original_result = original_model.simulate(0, 100, 1000, ['time', 'oxy'])
+# original import
+original_result = original_model.simulate(0, 100, 1000, ["time", "oxy"])
 original_tsc = TimeSeriesCollection.from_nd_array(original_result)
+original_ts_oxy = original_tsc.get_time_series("oxy")
+original_ts_oxy_timep = original_ts_oxy.time_points
+original_ts_oxy_val = original_ts_oxy.values
+
+# revised import
+revised_result = revised_model.simulate(0, 100, 1000, ["time", "oxy"])
+revised_tsc = TimeSeriesCollection.from_nd_array(revised_result)
+revised_ts_oxy = original_tsc.get_time_series("oxy")
+revised_ts_oxy_timep = revised_ts_oxy.time_points
+revised_ts_oxy_val = revised_ts_oxy.values
+
+#plot
+plt.plot(original_ts_oxy_timep, original_ts_oxy_val, label="original")
+plt.plot(revised_ts_oxy_timep, revised_ts_oxy_val, label="revised")
+plt.xlabel("Time")
+plt.ylabel("Concentration")
+plt.legend()
+plt.show()
