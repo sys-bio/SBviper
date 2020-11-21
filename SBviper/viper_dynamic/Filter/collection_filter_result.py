71b953a326b64e8e09df0ec455cfafe9b33e04a1
mastevb <bochenma@cs.washington.edu>
2020-11-21 14:43:43 -0800

    updated fr_frc.puml

--- SBviper/viper_dynamic/Filter/collection_filter_result.py
+++ SBviper/viper_dynamic/Filter/collection_filter_result.py
@@ -6,4 +6,19 @@ class FilterResultCollection:
     ----------
     _filter_results_dict : dict
 
+    Methods
+    -------
+    filters() : numpy.ndarray
+        Return an array of instances to filters that generated the filter results
+    filter_results() : numpy.ndarray
+        Return an array of filter results associated with this collection
+    size() : int
+        The size of the collection
+    add_filter_result(filter, filter_result) : void
+        Add a new FilterResult object of a filter to this collection
+    get_filter_result(filter) : FilterResult
+        Get the corresponding FilterResult object of the variable
+    __getitem__(filter)
+    __setitem__(filter, filter_result)
+
     """
