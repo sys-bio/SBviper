f22452af61cfa85714330f124f376f9f4f8b2a52
mastevb <bochenma@cs.washington.edu>
2020-11-22 10:12:17 -0800

    added from ant in main_runner.py

--- SBviper/viper_dynamic/util.py
+++ SBviper/viper_dynamic/util.py
@@ -3,6 +3,7 @@
 import os
 from SBviper.viper_dynamic.time_series.collection_time_series import \
     TimeSeriesCollection
+import tellurium as te
 
 
 def get_abs_path(filename):
@@ -23,8 +24,43 @@ def get_abs_path(filename):
     return os.path.join(dirname, filename)
 
 
-def get_tsc_from_SBML(original_path, revised_path):
-    pass
+def get_tsc_from_Ant(original_path, revised_path):
+    """
+    get tsc from Antimony file
+
+    Parameters
+    ----------
+    original_path: str
+        path to the original Antimony file
+    revised_path: str
+        path to the revised Antimony file
+
+    Raises
+    ------
+    ValueError:
+        invalid path
+
+    Returns
+    -------
+    TimeSeriesCollection:
+        original tsc
+    TimeSeriesCollection:
+        revised tsc
+    """
+    if not os.path.isfile(original_path) or not os.path.isfile(revised_path):
+        original_path = get_abs_path(original_path)
+        revised_path = get_abs_path(revised_path)
+        if not os.path.isfile(original_path) or not os.path.isfile(
+                revised_path):
+            raise ValueError
+    original_model = te.loada(original_path)
+    revised_model = te.loada(revised_path)
+    # TODO: Making an assumption here
+    original_result = original_model.simulate(0, 100, 1000)
+    revised_result = revised_model.simulate(0, 100, 1000)
+    original_tsc = TimeSeriesCollection.from_nd_array(original_result)
+    revised_tsc = TimeSeriesCollection.from_nd_array(revised_result)
+    return original_tsc, revised_tsc
 
 
 def get_tsc_from_CSV(original_path, revised_path):
@@ -58,4 +94,42 @@ def get_tsc_from_CSV(original_path, revised_path):
             raise ValueError
     original_tsc = TimeSeriesCollection.from_csv(original_path)
     revised_tsc = TimeSeriesCollection.from_csv(revised_path)
+    return original_tsc, revised_tsc
+
+def get_tsc_from_SBML(original_path, revised_path):
+    """
+    get tsc from SBML file
+
+    Parameters
+    ----------
+    original_path: str
+        path to the original SBML file
+    revised_path: str
+        path to the revised SBML file
+
+    Raises
+    ------
+    ValueError:
+        invalid path
+
+    Returns
+    -------
+    TimeSeriesCollection:
+        original tsc
+    TimeSeriesCollection:
+        revised tsc
+    """
+    if not os.path.isfile(original_path) or not os.path.isfile(revised_path):
+        original_path = get_abs_path(original_path)
+        revised_path = get_abs_path(revised_path)
+        if not os.path.isfile(original_path) or not os.path.isfile(
+                revised_path):
+            raise ValueError
+    original_model = te.loadSBMLModel(original_path)
+    revised_model = te.loadSBMLModel(revised_path)
+    # TODO: Making an assumption here
+    original_result = original_model.simulate(0, 100, 1000)
+    revised_result = revised_model.simulate(0, 100, 1000)
+    original_tsc = TimeSeriesCollection.from_nd_array(original_result)
+    revised_tsc = TimeSeriesCollection.from_nd_array(revised_result)
     return original_tsc, revised_tsc
--- main_runner.py
+++ main_runner.py
@@ -9,19 +9,23 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Detecting changes in models")
-    list_choices = ["SBML", "CSV"]
+    list_choices = ["SBML", "CSV", "Antimony"]
     parser.add_argument("Type", type=str, nargs="?", default="SBML",
-                        help="Are you using a CSV file or a SBML file?",
+                        help="Are you using a CSV file, a SBML model, or an "
+                             "Antimony model?",
                         choices=list_choices)
     args = parser.parse_args()
     path = args.Type
     original_path = input("The absolute path to the original model: ")
     revised_path = input("The absolute path to the revised model: ")
     if path == "SBML":
+        # TODO: Test this
         original_model, revised_model = get_tsc_from_SBML(original_path,
                                                           revised_path)
-    else:
+    elif path == "CSV":
         # TODO: Test this
         original_model, revised_model = get_tsc_from_CSV(original_path,
                                                          revised_path)
-
+    else: #Antimony
+        original_model, revised_model = get_tsc_from_Ant(original_path,
+                                                         revised_path)
